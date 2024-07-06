c45a33198c4146bd9814287337d067ca88cd198b
Angus McIntyre <angus@nomadcode.com>
2024-07-06 11:54:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# README
+
+## Overview
+
+This is a simple library for use with [`gulp`](https://gulpjs.org/) that provides some utility functions for validating JSON and other data files.
+
+This is NOT a finished library. It's a set of thrown-together functions for use in my own projects. If it's useful to you, that's great, but it's not a finished or polished product, and it's subject to alteration or even removal without notice.
+
+## Usage
+
+TBD
+
---
+++ index.mjs
@@ -0,0 +1,41 @@
+import { promises as fsPromises } from "fs";
+import Ajv from "ajv";
+import fancylog from "fancy-log";
+
+const loadJsonFile = async (filePath) => {
+  const theJson = await fsPromises.readFile(filePath, "utf8");
+  return JSON.parse(theJson);
+};
+
+/**
+ * Validate a JSON file against a JSON schema (https://json-schema.org/)
+ *
+ * @param {*} jsonFile
+ * @param {*} schemaFile
+ * @returns
+ */
+const validateJSON = async (jsonFile, schemaFile) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      fancylog.info(`Validating ${jsonFile} against ${schemaFile}`);
+      var theJson = await loadJsonFile(jsonFile);
+      var theSchema = await loadJsonFile(schemaFile);
+      const ajv = new Ajv();
+      const validate = ajv.compile(theSchema);
+      const valid = validate(theJson);
+      if (!valid) {
+        fancylog.warn(validate.errors);
+        reject(`'${jsonFile} contained errors`);
+      }
+      resolve(valid);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+/* ======================================================================
+ * EXPORTS
+ * ====================================================================== */
+
+export { validateJSON };
